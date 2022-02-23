89902ac0cc85fba8e8b48bea1f6079f9f74f17c0
Kirill <freshjs.ftd.@gmail.com>
2022-02-23 20:01:05 +0200

    Add element search methods

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,29 @@ window.document; // DOM
 
 document; // тоже DOM
 
-// что делать
+let clickCount = 0;
+
 function clickHandler() {
-  alert('на меня нажали');
+  alert(`Вы нажали ${(clickCount += 1)} раз`);
 }
 // кому
 const [button] = document.getElementsByTagName('button');
 // как
 button.addEventListener('click', clickHandler);
+
+/*
+  Сделайте чтобы по клику на кнопку
+  вам выводилось число кликов 
+  которое вы уже совершили
+*/
+
+// поиск элементов
+// document.getElementsByTagName - все элементы с определенным тегом
+const headers = document.getElementsByClassName('header-text');
+
+const nameInputs = document.getElementsByName('firstName');
+const textPar = document.getElementById('main-text');
+
+const elem = document.querySelector('.main-article > .text');
+const nameSelector = document.querySelector('[name]');
+const nameSelectors = document.querySelectorAll('[name]');
