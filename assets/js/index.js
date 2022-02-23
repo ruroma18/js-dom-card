fcef535dd137e086ae168d7fd5954e3459af3659
Kirill <freshjs.ftd.@gmail.com>
2022-02-23 20:43:24 +0200

    Add paragraph creation task

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,15 +11,9 @@ const [button] = document.getElementsByTagName('button');
 button.addEventListener('click', clickHandler);
 
 function clickHandler() {
-  button.textContent = `<h1>Меня нажали ${clickCount +=1} раз</h1>`;
+  button.textContent = `<h1>Меня нажали ${(clickCount += 1)} раз</h1>`;
   button.style.padding = `${clickCount}px`;
 }
-
-/*
-  Сделайте чтобы по клику на кнопку
-  вам выводилось число кликов 
-  которое вы уже совершили
-*/
 
 // поиск элементов
 // document.getElementsByTagName - все элементы с определенным тегом
@@ -31,3 +25,28 @@ const textPar = document.getElementById('main-text');
 const elem = document.querySelector('.main-article > .text');
 const nameSelector = document.querySelector('[name]');
 const nameSelectors = document.querySelectorAll('[name]');
+
+const taskBtn = document.querySelector('#addTaskBtn');
+
+const taskList = document.querySelector('#task-list');
+
+taskBtn.addEventListener('click', addTaskListener);
+
+let taskCounter = 1;
+
+function addTaskListener() {
+  const listItemElem = document.createElement('li');
+  listItemElem.textContent = `Task ${taskCounter++}`;
+  taskList.append(listItemElem);
+  console.log(listItemElem);
+}
+
+/*
+Создайте кнопку добавления сообщений
+кнпка будет добавлять елементы параграфов с текстом "сообщения"
+текст должен иметь синий цвет и размер шрифта должен быть 20 пикселей
+текст будет присеодинятся к элементу body
+*/
+
+
+// text-align -> textAlign
