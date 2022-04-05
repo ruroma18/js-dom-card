5c068c3a096875452abe06c50a8d50aae0b91462
Kirill <freshjs.ftd.@gmail.com>
2022-04-05 21:18:44 +0300

    Add event object example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,64 +1,19 @@
-let test = 1234;
+'use strict';
 
-// console.log(`log: ${test}`);
+const btn1 = document.getElementById('btn1');
+const article = document.querySelector('.main-article')
 
-// function log() {
-//   console.log(`log: ${test}`);
-// }
+const listener = (event) => {
+  console.group();
+  console.log(event.target); // куда кликнули
+  console.log(event.currentTarget); // тот чей обработчик
+  console.groupEnd();
+};
 
-function wrapper() {
-  let test = 43211;
-  // console.log(`wrapper: ${test}`);
+btn1.addEventListener('click', listener);
+article.addEventListener('click', listener);
+document.body.addEventListener('click', listener);
+document.addEventListener('click', listener);
 
-  function log() {
-    console.log(test);
-  }
 
-  log();
-}
-
-function makeCounter() {
-  let i = 0; // переменная в замыкании
-
-  function increment() {
-    debugger;
-    i++;
-    return i;
-  }
-
-  function decrement() {
-    debugger;
-    i--;
-    return i;
-  }
-
-  function getCounter() {
-    return i;
-  }
-
-  return {
-    increment: increment,
-    decrement: decrement,
-    getCounter: getCounter,
-  };
-}
-
-const obj = makeCounter();
-
-/*
-Создать функцию makeAdder, она принимает число Х
-и возвращать другую функцию 
-возвращаенная функция будет принимать число Y и должна 
-складывать Х c Y и возвращать результат
-
-const adder = makeAdder(100);
-
-adder(50) // 150
-adder(30) // 180
-
-*/
-
-const makeAdder = (x) => (y) => (x += y);
-
-const adder = makeAdder(100);
-const adder2 = makeAdder(-5000);
+// btn1.removeEventListener('click',listener);
