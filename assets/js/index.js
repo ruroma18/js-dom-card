fccdae7e6c1bd53435041e0d5136d4a077bb877f
Kirill <freshjs.ftd.@gmail.com>
2022-04-08 19:17:21 +0300

    Add solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,43 @@
 'use strict';
 
-// data-*
-
-const [btn1] = document.querySelectorAll('button');
-
-const attr1 = btn1.getAttribute('my-attr');
-
-const attr2 = btn1.getAttribute('data-my-attr');
-
-btn1.addEventListener('click', (e) => {
-  
-  const {
-    target: { dataset: {myAttr} },
-  } = e;
-
-  console.log(myAttr);
-});
-
 /*
   Есть див с конпками
   по нажатию на кнопку поменять цвет / цвет фона дива на тот
   который на кнопке указан
   информация про цвет кнопки хранится в data-* аттрибуте
-
 */
+
+// const div = document.getElementById('root');
+
+// function listener(e) {
+//   const {
+//     target: {
+//       dataset: { color },
+//       parentElement,
+//     },
+//   } = e;
+//   console.dir(e.target);
+//   parentElement.style.backgroundColor = color;
+// }
+
+// const btns = document.querySelectorAll('button');
+
+// btns.forEach((btn) => {
+//   btn.addEventListener('click', listener);
+// });
+
+function listener(e) {}
+
+const div = document.querySelector('#root');
+
+div.addEventListener('click', (e) => {
+  const {
+    currentTarget,
+    target: {
+      dataset: { color },
+    },
+  } = e;
+  if (color) {
+    currentTarget.style.backgroundColor = color;
+  }
+});
