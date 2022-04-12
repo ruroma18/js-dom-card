0c370b26d9d37037bc758fcf2a6599f2f6b32dcf
Kirill <freshjs.ftd.@gmail.com>
2022-04-12 20:53:42 +0300

    Add map

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -3,3 +3,14 @@ const CARD_CONSTANTS = {
   cardDescription:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum efficitur, ligula nec molestie viverra, lectus lacus scelerisque justo, ac placerat sem est id sapien. Aliquam erat volutpat. Nunc quis dolor neque. Phasellus sed purus vitae metus sagittis commodo eget sit amet urna. Aliquam erat volutpat. Pellentesque a mauris orci. Morbi dictum urna vel elit euismod, eu pellentesque erat venenatis. Praesent aliquet diam libero, eget egestas enim sagittis vitae',
 };
+
+const SUPPORTED_SOCIAL_NETWORKS = new Map([
+  [
+    'twitter.com',
+    {
+      src: 'https://w7.pngwing.com/pngs/872/50/png-transparent-computer-icons-social-media-logo-twitter-social-media-blue-logo-social-media-thumbnail.png',
+      alt: 'twitter link for',
+    },
+  ],
+  ['www.facebook.com', ],
+]);
